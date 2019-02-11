e5792d25ddbd8f9fbf8b0c5f8d1b01cad59b526d
github oka91 <oka.sma91@gmail.com>
2019-02-11 11:37:13 +0900

    データベースの文字コードをlatin1からutf8へ変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_01_23_020256) do
 
-  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
