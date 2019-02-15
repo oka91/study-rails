784e8b72c2ab155b126b6f6090a505a4d5563924
github oka91 <oka.sma91@gmail.com>
2019-02-15 15:25:23 +0900

    タスクモデルのタスク名に長さの制約を付与。３０文字

--- db/migrate/20190123020256_create_tasks.rb
+++ db/migrate/20190123020256_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name, null: false
+      t.string :name, null: false, limit: 30
       t.text :description
 
       t.timestamps
