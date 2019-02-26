33885d417bce73dab7abbc5122497a7107fd7eb8
github oka91 <oka.sma91@gmail.com>
2019-02-26 17:55:56 +0900

    タスクの名称がない場合、「名前なし」にする検証前の操作をコメントアウト

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
+  # before_validation :set_nameless_name
 
   validates :name, presence: true
   validates :name, length:{ maximum: 30 }
@@ -11,9 +11,9 @@ class Task < ApplicationRecord
 
   private
 
-  def set_nameless_name
-    self.name = '名前なし' if name.blank?
-  end
+  # def set_nameless_name
+  #   self.name = '名前なし' if name.blank?
+  # end
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
